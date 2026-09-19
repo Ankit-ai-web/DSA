425838b0d24bc95715f63c10e60bc59a5196b51d
Ankit-ai-web <ankitkr200ak@gmail.com>
2026-09-19 22:43:35 +0530

    Time: 7 ms (37.08%) | Memory: 45.6 MB (15.19%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,32 +1,30 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> stack = new Stack<>();
-        for(String token: tokens){
-            if(token.equals("+") || token.equals("-")||
-             token.equals("*") || token.equals("/")){
+        Stack<Integer>stack = new Stack<>();
+        for(String token:tokens){
+            if(token.equals("+")||
+            token.equals("-")||
+            token.equals("*")||
+            token.equals("/")){
                 int a = stack.pop();
                 int b = stack.pop();
-                switch(token){
-                    case "+":
-                        stack.push(b+a);
-                        break;
-
-                    case "-":
-                        stack.push(b-a);
-                        break;
-
-                    case "*":
-                        stack.push(b*a);
-                        break;
-
-                    case "/":
-                        stack.push(b/a);
-                        break;
+                int result = 0;
+                
+                if(token.equals("+")){
+                    result = a+b;
+                } else if(token.equals("-")){
+                    result = b-a;
+                } else if(token.equals("*")){
+                    result = a*b;
+                } else{
+                    result = b/a;
                 }
-            }else{
+                stack.push(result);
+            }
+            else{
                 stack.push(Integer.parseInt(token));
             }
         }
-        return stack.peek();
+        return stack.pop();
     }
 }
